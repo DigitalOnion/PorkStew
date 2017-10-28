52e8e64b59255397fcb7d5def3cddd70d0087102
DigitalOnion <luis.viruena@gmail.com>
2017-10-28 11:43:57 -0700

    Minimal functional GenericAdapter

--- app/src/main/java/com/outerspace/porkstew/adapter/GenericAdapter.java
+++ app/src/main/java/com/outerspace/porkstew/adapter/GenericAdapter.java
@@ -1,33 +1,51 @@
 package com.outerspace.porkstew.adapter;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.outerspace.porkstew.R;
+import com.outerspace.porkstew.model.Person;
+import com.outerspace.porkstew.viewmodel.ListItemViewModel;
 
 import java.util.ArrayList;
 
 public class GenericAdapter extends RecyclerView.Adapter<GenericAdapter.GenericViewHolder>{
-
+    private ListItemViewModel viewModel;
     private ArrayList<IGenericDataItem> dataItems;
+
+    public GenericAdapter(ListItemViewModel viewModel, ArrayList<IGenericDataItem> dataItems) {
+        this.viewModel = viewModel;
+        this.dataItems = dataItems;
+    }
 
     @Override
     public GenericViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater
+                .from(parent.getContext())
+                .inflate(R.layout.list_item, parent, false);
+        GenericViewHolder holder = new GenericViewHolder(view);
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(GenericViewHolder holder, int position) {
-
+        Person person = (Person) dataItems.get(position);
+        holder.txtFullname.setText(person.fullName());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return dataItems.size();
     }
 
     public class GenericViewHolder extends RecyclerView.ViewHolder {
+        public TextView txtFullname;
         public GenericViewHolder(View itemView) {
             super(itemView);
+            txtFullname = (TextView)  itemView.findViewById(R.id.text_fullname);
         }
     }
 
