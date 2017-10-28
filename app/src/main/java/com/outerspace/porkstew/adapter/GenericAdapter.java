1bab75d76e4e6cc08b705938f3c3e908283168e6
DigitalOnion <luis.viruena@gmail.com>
2017-10-28 12:13:19 -0700

    DataBinding in GenericAdapter... works, but is not yet finished

--- app/src/main/java/com/outerspace/porkstew/adapter/GenericAdapter.java
+++ app/src/main/java/com/outerspace/porkstew/adapter/GenericAdapter.java
@@ -1,12 +1,15 @@
 package com.outerspace.porkstew.adapter;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.android.databinding.library.baseAdapters.BR;
 import com.outerspace.porkstew.R;
+import com.outerspace.porkstew.databinding.ListItemBinding;
 import com.outerspace.porkstew.model.Person;
 import com.outerspace.porkstew.viewmodel.ListItemViewModel;
 
@@ -15,17 +18,19 @@ import java.util.ArrayList;
 public class GenericAdapter extends RecyclerView.Adapter<GenericAdapter.GenericViewHolder>{
     private ListItemViewModel viewModel;
     private ArrayList<IGenericDataItem> dataItems;
+    private int layoutId;
 
-    public GenericAdapter(ListItemViewModel viewModel, ArrayList<IGenericDataItem> dataItems) {
+    public GenericAdapter(ListItemViewModel viewModel, ArrayList<IGenericDataItem> dataItems, int layoutId) {
         this.viewModel = viewModel;
         this.dataItems = dataItems;
+        this.layoutId  = layoutId;
     }
 
     @Override
     public GenericViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater
                 .from(parent.getContext())
-                .inflate(R.layout.list_item, parent, false);
+                .inflate(layoutId, parent, false);
         GenericViewHolder holder = new GenericViewHolder(view);
         return holder;
     }
@@ -33,7 +38,8 @@ public class GenericAdapter extends RecyclerView.Adapter<GenericAdapter.GenericV
     @Override
     public void onBindViewHolder(GenericViewHolder holder, int position) {
         Person person = (Person) dataItems.get(position);
-        holder.txtFullname.setText(person.fullName());
+        holder.binding.setVariable(BR.list_item_view_model, viewModel);
+        // holder.txtFullname.setText(person.fullName());
     }
 
     @Override
@@ -42,10 +48,12 @@ public class GenericAdapter extends RecyclerView.Adapter<GenericAdapter.GenericV
     }
 
     public class GenericViewHolder extends RecyclerView.ViewHolder {
-        public TextView txtFullname;
+        public ListItemBinding binding;
+        //public TextView txtFullname;
         public GenericViewHolder(View itemView) {
             super(itemView);
-            txtFullname = (TextView)  itemView.findViewById(R.id.text_fullname);
+            binding = DataBindingUtil.bind(itemView);
+            //txtFullname = (TextView)  itemView.findViewById(R.id.text_fullname);
         }
     }
 
