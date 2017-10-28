b0e616474fdfc25ae04bdb21bfcaff20837801e2
DigitalOnion <luis.viruena@gmail.com>
2017-10-28 11:49:31 -0700

    Replaced MyAdapter for GenericAdapter

--- app/src/main/java/com/outerspace/porkstew/MainActivity.java
+++ app/src/main/java/com/outerspace/porkstew/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import com.outerspace.porkstew.adapter.GenericAdapter;
+import com.outerspace.porkstew.adapter.IGenericDataItem;
 import com.outerspace.porkstew.adapter.MyAdapter;
 import com.outerspace.porkstew.model.Person;
 import com.outerspace.porkstew.model.PersonList;
@@ -27,8 +29,14 @@ public class MainActivity extends AppCompatActivity {
         ArrayList<Person> persons = personList.getList();
         MyAdapter adapter = new MyAdapter(persons);
 
+        ArrayList<IGenericDataItem> dataItems = new ArrayList<>();
+        for(Person person: persons)
+            dataItems.add(person);
+        GenericAdapter genericAdapter = new GenericAdapter(null, dataItems);
+
         RecyclerView recycler = (RecyclerView) findViewById(R.id.recycler);
         recycler.setLayoutManager(new LinearLayoutManager(this));
-        recycler.setAdapter(adapter);
+        // recycler.setAdapter(adapter);
+        recycler.setAdapter(genericAdapter);
     }
 }
