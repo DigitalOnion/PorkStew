dd9de2c84a69595714cc866781066aa2b0f68970
DigitalOnion <luis.viruena@gmail.com>
2017-10-28 18:06:13 -0700

    Add ViewModel as a Factory to the GenericAdapter invocation

--- app/src/main/java/com/outerspace/porkstew/MainActivity.java
+++ app/src/main/java/com/outerspace/porkstew/MainActivity.java
@@ -1,5 +1,6 @@
 package com.outerspace.porkstew;
 
+import android.app.LauncherActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,6 +11,7 @@ import com.outerspace.porkstew.adapter.IGenericDataItem;
 import com.outerspace.porkstew.adapter.MyAdapter;
 import com.outerspace.porkstew.model.Person;
 import com.outerspace.porkstew.model.PersonList;
+import com.outerspace.porkstew.viewmodel.ListItemViewModel;
 
 import java.util.ArrayList;
 
@@ -28,11 +30,12 @@ public class MainActivity extends AppCompatActivity {
         PersonList personList = new PersonList();
         ArrayList<Person> persons = personList.getList();
         MyAdapter adapter = new MyAdapter(persons);
+        ListItemViewModel viewModelFactory = new ListItemViewModel();
 
         ArrayList<IGenericDataItem> dataItems = new ArrayList<>();
         for(Person person: persons)
             dataItems.add(person);
-        GenericAdapter genericAdapter = new GenericAdapter(null, dataItems, R.layout.list_item);
+        GenericAdapter genericAdapter = new GenericAdapter(viewModelFactory, dataItems, R.layout.list_item);
 
         RecyclerView recycler = (RecyclerView) findViewById(R.id.recycler);
         recycler.setLayoutManager(new LinearLayoutManager(this));
